155c85218711fb6e1825567fa365332ec63cc765
ozzvaa <matevz.ozvatic@gmail.com>
2025-12-13 17:55:43 +0100

    Init commit

---
+++ core/src/main/java/si/um/feri/tevzki/ShovelGame.java
@@ -0,0 +1,11 @@
+package si.um.feri.tevzki;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class ShovelGame extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
